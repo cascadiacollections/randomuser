d7799c5bd64533feb970f60f206a9daa741a4d54
Kevin T. Coughlin <kevin@cascadiacollections.com>
2021-09-11 19:58:08 -0700

    Clean-up tests

--- test/client.test.js
+++ test/client.test.js
@@ -1,30 +1,31 @@
 // @ts-check
 'use strict';
 
-var RandomUser = require('..')
-  , should = require('should')
-  , r;
+const RandomUser = require('..')
+  , should = require('should');
 
-beforeEach(function(){
-  r = new RandomUser();
-});
+describe('r.getUsers()', () => {
+  let r;
 
-describe('r.getUsers()', function() {
-  it('should throw error if callback is not typeof function', function () {
+  beforeEach(() => {
+    r = new RandomUser();
+  });
+
+  it('should throw error if callback is not typeof function', () => {
     (function() {
       r.getUsers(null, null, 'string');
     }).should.throwError('ERROR: Invalid callback function.');
   });
 
-  it('should successfully complete a request without params', function (done) {
-    r.getUsers(function(data) {
+  it('should successfully complete a request without params', (done) => {
+    r.getUsers(data => {
       should.exist(data);
       done();
     });
   });
 
-  it('should successfully complete a request with params', function (done) {
-    r.getUsers({ seed: "foxie", results: 5, gender: "male" }, function(data) {
+  it('should successfully complete a request with params', (done) => {
+    r.getUsers({ seed: "foxie", results: 5, gender: "male" }, data => {
       should.exist(data);
       done();
     });
